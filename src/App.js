9321f0f2dfbe9465f52a3ce32e683e9ccfbb1844
GonzaCoder <gonzalo19_10@hotmail.com>
2022-02-21 18:11:02 -0300

    Primer commit prueba

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header.js"
+import Footer from "./Footer.js"
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+   return (
+      <header>
+         <h1>Titulo</h1>
+         <nav>
+            <a>1</a>
+            <a>2</a>
+            <a>3</a>
+         </nav>
+      </header>
+   )
+}
+
+export default Header
